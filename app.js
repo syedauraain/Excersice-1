3efc7d885f6e68ddda6bb258b4a045cb6815606c
syedauraain <160678874+syedauraain@users.noreply.github.com>
2024-04-18 14:34:33 -0700

    Did personal message

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+let message = "hello world";
+console.log(message);
+let latter = "kiya haal hyn";
+console.log(latter);
+let letter1 = "or sunayin";
+console.log(letter1);
+let massage = "mai theek ap sunayin";
+console.log(massage);
